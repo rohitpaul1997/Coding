ad23c1f731bcd2623ca522eb5008bf5026707f09
Rohit Paul <rohit.paul1997@gamil.com>
2023-01-03 21:02:50 +0530

    03-01-2023

---
+++ LeetCode/ffdhdh.py
@@ -0,0 +1,13 @@
+def minDeletionSize(strs: list[str]) -> int:
+        c=0
+        for col in range(len(strs[0])):
+            for row in range(len(strs)-1):
+                if ord(strs[row][col])>ord(strs[row+1][col]):
+                    # print(strs[row][col],strs[row+1][col])
+                    print(ord(strs[row][col]))
+                    print(ord(strs[row+1][col]))
+                    c+=1
+                    break
+        return c
+
+print(minDeletionSize(["cba","daf","ghi"]))
